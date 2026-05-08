eecf6a7ed1e913200ea90e42cfae3139dfb41387
riti <abidariti690@gmail.com>
2026-05-08 21:17:23 +0600

    added index.aspx

--- SGIPC-Portfolio/Site.Master.designer.cs
+++ SGIPC-Portfolio/Site.Master.designer.cs
@@ -15,21 +15,21 @@ namespace SGIPC_Portfolio
     {
 
         /// <summary>
-        /// HeadContent control.
+        /// Head control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Head;
 
         /// <summary>
-        /// MainContent control.
+        /// body control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder body;
     }
 }
--- SGIPC-Portfolio/index.aspx
+++ SGIPC-Portfolio/index.aspx
@@ -1,3 +1,10 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="SGIPC_Portfolio.index" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+   
+
+   <asp:Content ID="Content1" ContentPlaceHolderID="body" runat="server">
+       <div class="img">
+
+       </div>
+    
 </asp:Content>
